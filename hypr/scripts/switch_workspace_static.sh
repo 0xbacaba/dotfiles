e4b7189e680f0af52645ffe6e4d30f6e0324fcd8
Joshi <joshua@lipps.de>
2025-10-22 11:42:57 +0200

    [hypr] add keybind to move to the active monitors 'origin workspace'

--- hypr/scripts/switch_workspace_static.sh
+++ hypr/scripts/switch_workspace_static.sh
@@ -3,7 +3,7 @@
 # Increments/Decrements the workspace number by the number of monitors.
 # This ensures each monitor is assigned its own workspaces
 
-source utils.sh
+source `dirname $0`/utils.sh
 
 require hyprctl
 require jq
@@ -15,9 +15,15 @@ if [ "$1" == "l" ]; then
 	new_workspace=$((current_workspace - active_monitors))
 elif [ "$1" == "r" ]; then
 	new_workspace=$((current_workspace + active_monitors))
+elif [ "$1" == "o" ]; then
+	active_monitor=`hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .description'`
+	new_workspace=`hyprctl workspacerules -j | jq -r --arg mon_desc "$active_monitor" '.[] | select(.monitor == "desc:" + $mon_desc) | .workspaceString'`
+	echo "moving to workspace $new_workspace"
 else
-	echo "usage: $0 l|r [true|false]"
-	echo "example: $0 r true -> move active window to next workspace"
+	echo "usage: $0 l|r|o [true|false]"
+	echo "examples: "
+	echo "  $0 r true -> move active window to next workspace"
+	echo "  $0 o -> switch to the current monitors origin workspace without moving a window"
 	exit
 fi
 
