3250dad391fad59ca28e89bdbe5dcfea038ef8fb
Joshi <joshua@lipps.de>
2026-05-01 18:06:37 +0200

    [hypr] fix suspend using power script

--- hypr/scripts/power.sh
+++ hypr/scripts/power.sh
@@ -36,7 +36,7 @@ case "$selected" in
 		;;
 	`raw ${options[1]}`)
 		echo "[suspend] systemctl suspend"
-		hyprlock --immediate &
+		hyprlock --grace 0 &
 		systemctl suspend
 		;;
 	`raw ${options[2]}`)
