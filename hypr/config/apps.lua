aa9608113cf372c109ea386792b2ca5278f76237
Joshi <joshua@lipps.de>
2026-07-09 16:53:48 +0200

    [hypr] add QT_QPA_PLATFORMTHEME and XDG_MENU_PREFIX env variables

--- hypr/config/apps.lua
+++ hypr/config/apps.lua
@@ -1,35 +1,38 @@
 local options = {
-  terminal = "konsole",
-  fileManager = "dolphin",
-  menu = "vicinae toggle",
+	terminal = "konsole",
+	fileManager = "dolphin",
+	menu = "vicinae toggle",
 
-  -- This    vvvvvvvvvvvvvvvv is required to use the kde filepicker in firefox-based browsers
-  browser = "GTK_USE_PORTAL=1 zen-browser",
-  spotifyClient = "spotify",
-  discordClient = "vesktop",
-  screenshotUtility = "hyprshot -m region --clipboard-only -s -z",
+	-- This    vvvvvvvvvvvvvvvv is required to use the kde filepicker in firefox-based browsers
+	browser = "GTK_USE_PORTAL=1 zen-browser",
+	spotifyClient = "spotify",
+	discordClient = "vesktop",
+	screenshotUtility = "hyprshot -m region --clipboard-only -s -z",
 
-  clipboardWatch = "clipse -listen",
-  clipboardShow = "alacritty --class clipse -e 'clipse'",
+	clipboardWatch = "clipse -listen",
+	clipboardShow = "alacritty --class clipse -e 'clipse'",
 
-  screenlockUtility = "hyprlock",
+	screenlockUtility = "hyprlock",
 }
 
 local startup = {
-  "waybar",
-  "hyprpaper",
-  "dunst",
-  "/usr/bin/kwalletd6",
-  "vorta",
-  options.clipboardWatch,
-  "/usr/lib/polkit-kde-authentication-agent-1",
-  "vicinae server",
+	"waybar",
+	"hyprpaper",
+	"dunst",
+	"/usr/bin/kwalletd6",
+	"vorta",
+	options.clipboardWatch,
+	"/usr/lib/polkit-kde-authentication-agent-1",
+	"vicinae server",
 }
 
 hl.on("hyprland.start", function()
-  for _, app in ipairs(startup) do
-    hl.exec_cmd(app)
-  end
+	for _, app in ipairs(startup) do
+		hl.exec_cmd(app)
+	end
 end)
+
+hl.env("QT_QPA_PLATFORMTHEME", "kde")
+hl.env("XDG_MENU_PREFIX", "arch-")
 
 return options
