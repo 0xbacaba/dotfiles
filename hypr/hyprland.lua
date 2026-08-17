0dc8a01fb2d7e51082f8c8cd726b9ac45cf2f68d
Joshi <joshua@lipps.de>
2026-08-17 14:54:34 +0200

    [hypr] add automatic workspace configuration

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -2,5 +2,13 @@ require("config.apps")
 require("config.binds")
 require("config.default")
 require("config.rules")
-require("monitors")
-require("workspaces")
+
+local monitors, err = pcall(require, "monitors")
+if not monitors then
+	hl.notification.create({ text = "No monitor config found: " .. err, timeout = 5000, color = "#ff4f00" })
+end
+
+local rules = require("config.workspaces")
+for _, rule in ipairs(rules) do
+	hl.workspace_rule(rule)
+end
