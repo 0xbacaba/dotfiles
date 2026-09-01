6bd08223d309228efeaf863e420dcb9c200fd0d5
Joshi <joshua@lipps.de>
2026-09-01 11:53:16 +0200

    [hypr] reorganize workspaces when monitors change

--- hypr/config/workspaces.lua
+++ hypr/config/workspaces.lua
@@ -1,8 +1,5 @@
 -- workspaces are ordered by distance to the builtin monitor. If the builtin monitor was not detected, the workspaces are ordered from left to right.
 
-local builtin = hl.get_monitor("eDP-1")
-local monitors = hl.get_monitors()
-
 ---return the center point of the given monitor
 ---@param mon HL.Monitor
 local function center(mon)
@@ -23,30 +20,65 @@ local function dist(a, b)
 	return math.sqrt(dx ^ 2 + dy ^ 2)
 end
 
-if builtin then
-	table.sort(monitors, function(a, b)
-		-- monitors are ordered by distance to builtin
-		return dist(builtin, a) < dist(builtin, b)
-	end)
-else
-	table.sort(monitors, function(a, b)
-		-- monitors are ordered from left to right
-		return a.x < b.x
-	end)
-end
-
 ---@class WorkspaceRule
 ---@field workspace string
 ---@field monitor string
 
+local builtin = hl.get_monitor("eDP-1")
+local sorter
+if builtin then
+	sorter = function(a, b)
+		-- monitors are ordered by distance to builtin
+		return dist(builtin, a) < dist(builtin, b)
+	end
+else
+	sorter = function(a, b)
+		-- monitors are ordered from left to right
+		return a.x < b.x
+	end
+end
+
 ---@type WorkspaceRule[]
 local rules = {}
-for i, monitor in ipairs(monitors) do
-	table.insert(rules, {
-		workspace = tostring(i),
-		monitor = "desc:" .. monitor.description,
-	})
+
+local function reorganize()
+	local monitors = hl.get_monitors()
+	table.sort(monitors, sorter)
+
+	rules = {}
+	for i, monitor in ipairs(monitors) do
+		table.insert(rules, {
+			workspace = tostring(i),
+			monitor = "desc:" .. monitor.description,
+		})
+	end
+
+	for _, rule in ipairs(rules) do
+		hl.workspace_rule({
+			workspace = rule.workspace,
+			monitor = rule.monitor,
+		})
+
+		-- move all workspaces to their dedicated monitor
+		local monitor_count = #hl.get_monitors()
+		for _, workspace in ipairs(hl.get_workspaces()) do
+			local workspace_monitor_index = tonumber(workspace.name) % monitor_count
+			local rule_monitor_index = tonumber(rule.workspace) % monitor_count
+
+			if workspace_monitor_index == rule_monitor_index then
+				hl.dispatch(hl.dsp.workspace.move({
+					workspace = workspace,
+					monitor = rule.monitor,
+				}))
+			end
+		end
+	end
 end
+
+hl.on("hyprland.start", reorganize)
+hl.on("monitor.added", reorganize)
+hl.on("monitor.removed", reorganize)
+hl.on("monitor.layout_changed", reorganize)
 
 ---Find the workspace rule of the given monitor
 ---@param monitor HL.Monitor
--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -8,7 +8,4 @@ if not monitors then
 	hl.notification.create({ text = "No monitor config found: " .. err, timeout = 5000, color = "#ff4f00" })
 end
 
-local rules = require("config.workspaces")
-for _, rule in ipairs(rules) do
-	hl.workspace_rule(rule)
-end
+require("config.workspaces")
